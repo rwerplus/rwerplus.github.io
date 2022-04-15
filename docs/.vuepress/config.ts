62b2f97efbde3baed485fb1544ab278ad2818ce9
kenny yori <localfeng@163.com>
2022-04-15 10:26:10 +0800

    feat: 增加前端学习笔记文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,10 +28,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         text: '前端',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-        items: [
-          { text: 'HTML相关', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS相关', link: '/pages/0a83b083bdf257cb/' },
-        ]
+        items: []
       },
       {
         text: '开源',
