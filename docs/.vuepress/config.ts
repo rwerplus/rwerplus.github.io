7865c5c6b664fdb6355a294f6f6f105fb83c748c
Peak Wong <layoutfeng@gmail.com>
2022-11-18 14:11:20 +0800

    èµfeat: 升级依赖

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -43,6 +43,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: 'Java程序员', link: '/pages/e58ecf/' },
         ],
       },
       {
@@ -50,6 +51,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/pages/beb6c0bd8a66cea6/',
       },
       { text: '关于', link: '/about/' },
+      { text: '随笔', link: '/pages/f2e63f' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
